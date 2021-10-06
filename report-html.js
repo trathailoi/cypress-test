15c5e9df392aa76d64da7179e4209d12bb22238f
thang.cao <thang.cao@9thwonder.com>
2021-10-06 09:02:51 +0700

    update generate report

--- report-html.js
+++ report-html.js
@@ -1,12 +1,30 @@
 const reporter = require("cucumber-html-reporter")
 
-const options = {
-  theme: "bootstrap",
-  jsonFile: "cypress/reports/cucumber-json/1.feature.json",
-  output: "cypress/reports/cucumber_report.html",
-  reportSuiteAsScenarios: true,
-  scenarioTimestamp: true,
-  launchReport: true
-}
+const cucumberJsonFolder = './cypress/reports/cucumber-json';
+const fs = require('fs');
 
-reporter.generate(options)
+let jsonData = []
+fs.readdirSync(cucumberJsonFolder).forEach(file => {
+  const featureData = require(`${cucumberJsonFolder}/${file}`)
+  jsonData = [...jsonData, ...featureData]
+});
+
+const reportDataPath = `${cucumberJsonFolder}/report-data.json`
+
+fs.writeFile(reportDataPath, JSON.stringify(jsonData, 0, 2), function (err) {
+  if (err) return console.log('error generate report data json', err)
+  console.log(`File report data json exported! Path: ${reportDataPath}`)
+  
+  console.log(`Report is generating....`)
+  const options = {
+    theme: "bootstrap",
+    jsonFile: reportDataPath,
+    output: `cypress/reports/html/cucumber_report_${new Date().getTime()}.html`,
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    launchReport: true
+  }
+  
+  reporter.generate(options)
+  console.log(`Report is generated!`)
+})
